a700662f53c45a63d61f9d25c9d8c716b16c01d9
Renan Araujo <renan290812@gmail.com>
2024-11-19 14:28:23 -0300

    dfdf

--- script.py
+++ script.py
@@ -1,23 +1,24 @@
-import os
 from telethon import TelegramClient, events
+import os
 
-# Lê as credenciais das variáveis de ambiente
-api_id = os.getenv('API_ID')  # Defina a variável de ambiente API_ID
-api_hash = os.getenv('API_HASH')  # Defina a variável de ambiente API_HASH
-bot_token = os.getenv('BOT_TOKEN')  # Defina a variável de ambiente BOT_TOKEN
+# Insira suas credenciais do Telegram
+api_id = '24446798'
+api_hash = 'ee38eed42c07719909e35a96c096e3be'
+phone = '+5511970640032'  # Opcional, necessário para a primeira autorização
+keywords = ['꧁ঔৣ☬✞ 𝙄𝙉𝙁𝙊. 𝘽𝙇𝘼𝘿𝙀', '꧁ঔৣ☬✞ 𝙄𝙉𝙁𝙊. 𝘾𝙇𝙐𝘽 - 𝙄𝙋𝙏𝙑']
+destination_chat_id = 'rbplay27'  # Pode ser um chat privado ou outro canal/grupo
 
-# Inicializa o cliente do Telethon com o bot token
-client = TelegramClient('session_name', api_id, api_hash).start(bot_token=bot_token)
+# Inicializa o cliente
+client = TelegramClient('session_name', api_id, api_hash)
 
 # Evento para monitorar mensagens em um canal
 @client.on(events.NewMessage(chats='bigger_servidor'))
 async def handler(event):
-    keywords = ['꧁ঔৣ☬✞ 𝙄𝙉𝙁𝙊. 𝘽𝙇𝘼𝘿𝙀', '꧁ঔৣ☬✞ 𝙄𝙉𝙁𝙊. 𝘾𝙇𝙐𝘽 - 𝙄𝙋𝙏𝙑']
-    destination_chat_id = 'rbplay27'  # Pode ser um chat privado ou outro canal/grupo
+    # Verifica se alguma das palavras-chave está presente no texto da mensagem
     if any(keyword.lower() in event.raw_text.lower() for keyword in keywords):
         print("Palavra-chave encontrada! Enviando a mensagem...")
         await client.send_message(destination_chat_id, event.message)
 
-# Executa o cliente
+# Executa o cliente (não precisa de Flask ou outro servidor web)
 with client:
     client.run_until_disconnected()
